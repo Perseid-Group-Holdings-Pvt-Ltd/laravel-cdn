73ed83effaad22f248624b8ecdac8b5240c630a4
Yash Tiwari <me.yashtiwari@gmail.com>
2026-04-23 15:13:05 +0530

    Add Asset, Cdn, and CdnFacade classes for asset management and CDN integration

---
+++ src/Asset.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace Perseid\LaravelCdn;
+
+use Perseid\LaravelCdn\Contracts\AssetInterface;
+
+/**
+ * Class Asset used to parse and hold all assets and
+ * paths related data and configurations.
+ *
+ * @category DTO
+ */
+class Asset implements AssetInterface
+{
+    /*
+     * Allowed assets for upload (found in included_directories)
+     *
+     * @var Collection
+     */
+    public $assets;
+
+    /**
+     * default [include] configurations.
+     */
+    protected array $default_include = [
+        'directories' => ['public'],
+        'extensions' => [],
+        'patterns' => [],
+    ];
+
+    /**
+     * default [exclude] configurations.
+     */
+    protected array $default_exclude = [
+        'directories' => [],
+        'files' => [],
+        'extensions' => [],
+        'patterns' => [],
+        'hidden' => true,
+    ];
+
+    protected array $included_directories;
+
+    protected array $included_files;
+
+    protected array $included_extensions;
+
+    protected array $included_patterns;
+
+    protected array $excluded_directories;
+
+    protected array $excluded_files;
+
+    protected array $excluded_extensions;
+
+    protected array $excluded_patterns;
+
+    /*
+     * @var boolean
+     */
+    protected mixed $exclude_hidden;
+
+    /**
+     * build an Asset object that contains the assets related configurations.
+     *
+     * @param  array  $configurations
+     * @return $this
+     */
+    public function init($configurations = [])
+    {
+        $this->parseAndFillConfiguration($configurations);
+
+        $this->included_directories = $this->default_include['directories'];
+        $this->included_extensions = $this->default_include['extensions'];
+        $this->included_patterns = $this->default_include['patterns'];
+
+        $this->excluded_directories = $this->default_exclude['directories'];
+        $this->excluded_files = $this->default_exclude['files'];
+        $this->excluded_extensions = $this->default_exclude['extensions'];
+        $this->excluded_patterns = $this->default_exclude['patterns'];
+        $this->exclude_hidden = $this->default_exclude['hidden'];
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getIncludedDirectories()
+    {
+        return $this->included_directories;
+    }
+
+    /**
+     * @return array
+     */
+    public function getIncludedExtensions()
+    {
+        return $this->included_extensions;
+    }
+
+    /**
+     * @return array
+     */
+    public function getIncludedPatterns()
+    {
+        return $this->included_patterns;
+    }
+
+    /**
+     * @return array
+     */
+    public function getExcludedDirectories()
+    {
+        return $this->excluded_directories;
+    }
+
+    /**
+     * @return array
+     */
+    public function getExcludedFiles()
+    {
+        return $this->excluded_files;
+    }
+
+    /**
+     * @return array
+     */
+    public function getExcludedExtensions()
+    {
+        return $this->excluded_extensions;
+    }
+
+    /**
+     * @return array
+     */
+    public function getExcludedPatterns()
+    {
+        return $this->excluded_patterns;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getAssets()
+    {
+        return $this->assets;
+    }
+
+    /**
+     * @param  mixed  $assets
+     */
+    public function setAssets($assets)
+    {
+        $this->assets = $assets;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getExcludeHidden()
+    {
+        return $this->exclude_hidden;
+    }
+
+    /**
+     * Check if the config file has any missed attribute, and if any attribute
+     * is missed will be overridden by a default attribute defined in this class.
+     */
+    private function parseAndFillConfiguration($configurations)
+    {
+        $this->default_include = isset($configurations['include']) ?
+            array_merge($this->default_include, $configurations['include']) : $this->default_include;
+
+        $this->default_exclude = isset($configurations['exclude']) ?
+            array_merge($this->default_exclude, $configurations['exclude']) : $this->default_exclude;
+    }
+}
---
+++ src/Cdn.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Perseid\LaravelCdn;
+
+use Perseid\LaravelCdn\Contracts\AssetInterface;
+use Perseid\LaravelCdn\Contracts\CdnHelperInterface;
+use Perseid\LaravelCdn\Contracts\CdnInterface;
+use Perseid\LaravelCdn\Contracts\FinderInterface;
+use Perseid\LaravelCdn\Contracts\ProviderFactoryInterface;
+
+/**
+ * Class Cdn is the manager and the main class of this package.
+ *
+ * @category Main Class
+ */
+class Cdn implements CdnInterface
+{
+    /**
+     * An instance of the finder class.
+     *
+     * @var Contracts\
+     */
+    protected $finder;
+
+    /**
+     * The object that will hold the assets configurations
+     * and the paths of the assets.
+     *
+     * @var Contracts\AssetInterface
+     */
+    protected $asset_holder;
+
+    protected $provider_factory;
+
+    protected $helper;
+
+    /**
+     * @internal param \app\CDN\Repository $configurations
+     */
+    public function __construct(
+        FinderInterface $finder,
+        AssetInterface $asset_holder,
+        ProviderFactoryInterface $provider_factory,
+        CdnHelperInterface $helper
+    ) {
+        $this->finder = $finder;
+        $this->asset_holder = $asset_holder;
+        $this->provider_factory = $provider_factory;
+        $this->helper = $helper;
+    }
+
+    /**
+     * Will be called from the PushCommand class on Fire().
+     */
+    public function push()
+    {
+        // return the configurations from the config file
+        $configurations = $this->helper->getConfigurations();
+
+        // Initialize an instance of the asset holder to read the configurations
+        // then call the read(), to return all the allowed assets as a collection of files objects
+        $assets = $this->finder->read($this->asset_holder->init($configurations));
+
+        // store the returned assets in the instance of the asset holder as collection of paths
+        $this->asset_holder->setAssets($assets);
+
+        // return an instance of the corresponding Provider concrete according to the configuration
+        $provider = $this->provider_factory->create($configurations);
+
+        return $provider->upload($this->asset_holder->getAssets());
+    }
+
+    /**
+     * Will be called from the EmptyCommand class on Fire().
+     */
+    public function emptyBucket()
+    {
+        // return the configurations from the config file
+        $configurations = $this->helper->getConfigurations();
+
+        // return an instance of the corresponding Provider concrete according to the configuration
+        $provider = $this->provider_factory->create($configurations);
+
+        return $provider->emptyBucket();
+    }
+}
+
